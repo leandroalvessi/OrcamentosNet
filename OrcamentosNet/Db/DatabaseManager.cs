0a3139bba3b3dba619f9e8b14c5a17bdb22e8a97
Leandro Alves da Silva <leandroalvespro@icloud.com>
2024-03-09 22:47:07 -0300

    #02 - Instalando pacotes

---
+++ OrcamentosNet/Db/DatabaseManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrcamentosNet.Db
+{
+    internal class DatabaseManager
+    {
+        public void CriarBancoDados()
+        {
+            string executableLocation = Assembly.GetExecutingAssembly().Location;
+            string executableDirectory = Path.GetDirectoryName(executableLocation);
+            string databasePath = Path.Combine(executableDirectory, "database.db");
+
+            if (!File.Exists(databasePath))
+            {
+                using (File.Create(databasePath)) { }
+
+                Console.WriteLine("Arquivo do banco de dados e tabela criados com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("O arquivo do banco de dados já existe.");
+            }
+        }
+    }
+}
---
+++ OrcamentosNet/Db/SQLiteConnectionManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrcamentosNet.Db
+{
+    internal class SQLiteConnectionManager
+    {
+        private string databasePath;
+        private string connectionString;
+
+        public SQLiteConnectionManager()
+        {
+            databasePath = Path.Combine(Directory.GetCurrentDirectory(), "database.db");
+            SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
+            connectionString = $"Data Source={databasePath}";
+        }
+
+        public SqliteConnection OpenConnection()
+        {
+            SqliteConnection connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+
+        public void CloseConnection(SqliteConnection connection)
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
